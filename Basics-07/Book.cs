0ea8690cb4f80388a2d8a46dede3928b6fdcfcfc
ali kamal <ali123kamal12@gmail.com>
2026-09-17 21:06:45 +0300

    second answer

--- Basics-07/Book.cs
+++ Basics-07/Book.cs
@@ -8,7 +8,11 @@ namespace Basics_07
     {
         #region answer one
         // I didn't write private access modefier because it's private by default
-        string password = "secret"; 
+        //string password = "secret";
+        #endregion
+
+        #region second answer
+        internal int copiesInStock = 5;
         #endregion
     }
 }
--- Basics-07/Program.cs
+++ Basics-07/Program.cs
@@ -1,14 +1,19 @@
-﻿namespace Basics_07
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            #region answer one
-            //it can be seen here because it can be accesed in book class only because it private field
-            Book book = new Book();
+﻿namespace Basics_07;
 
-            #endregion
-        }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region answer one
+        //it can be seen here because it can be accesed in book class only because it private field
+        //Book book = new Book();
+
+        #endregion
+
+        #region second answer
+        Book book = new Book();
+        //it can be accessed here because it's internal so it can accessed across the same project
+        Console.WriteLine(book.copiesInStock);
+        #endregion
     }
 }
